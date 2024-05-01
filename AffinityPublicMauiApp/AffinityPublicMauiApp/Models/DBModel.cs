70b767822f39badb4ce83db7596e7450a9fb81b2
Dj Williams <duran.evan.williams@gmail.com>
2024-05-01 18:42:38 -0400

    - Begin stubbing out models for affinity items to show/bind and database - Setup CommunityToolkit, begin using ObservableProperty - Continue stubbing out landing page with drone/vr projects

---
+++ AffinityPublicMauiApp/AffinityPublicMauiApp/Models/DBModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AffinityPublicMauiApp.Models
+{
+    public interface IDatabase
+    {
+        List<T> Get<T>(string key);
+        void AddItem<T>(T item);
+    }
+    public class DBModel : IDatabase
+    {
+        public List<T> Get<T>(string key) { 
+            // TODO: Get valz from db
+            return new List<T>();
+        }
+
+        public void AddItem<T>(T item)
+        {
+            // TODO: Add item to db
+        }
+    }
+}
