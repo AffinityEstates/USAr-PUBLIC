9053e85babab7d664173c364a375c2bb27e4aa53
Dj Williams <duran.evan.williams@gmail.com>
2024-06-04 18:08:07 -0400

    - No major code changes, trying to figure out what's wrong with the android emulator

--- AffinityPublicMauiApp/AffinityPublicMauiApp/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/AffinityPublicMauiApp.AssemblyInfo.cs
+++ AffinityPublicMauiApp/AffinityPublicMauiApp/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/AffinityPublicMauiApp.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AffinityPublicMauiApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+3cca592978fc26fd37c440a9493668b7e2afa0db")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+a469823962c2c6efcc9f4923c543032ef068baf6")]
 [assembly: System.Reflection.AssemblyProductAttribute("AffinityPublicMauiApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AffinityPublicMauiApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
